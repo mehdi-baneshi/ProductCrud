dfe1b9ee6c405e444472d03f06ff2557a62d39ee
mehdi-baneshi <mehdibaneshi1373@gmail.com>
2023-06-26 14:41:25 +0330

    create domain project

---
+++ src/ProductCrud.Domain/Common/BaseDomainEntity.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProductCrud.Domain.Common
+{
+    public abstract class BaseDomainEntity
+    {
+        public int Id { get; set; }
+        [Column(TypeName = "varchar(50)")]
+        public string CreatedBy { get; set; }
+    }
+}
---
+++ src/ProductCrud.Domain/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using ProductCrud.Domain.Common;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProductCrud.Domain.Entities
+{
+    public class Product : BaseDomainEntity
+    {
+        [Column(TypeName = "nvarchar(200)")]
+        public string Name { get; set; }
+        [Column(TypeName = "DateTime2")]
+        public DateTime ProduceDate { get; set; }
+        [Column(TypeName = "varchar(15)")]
+        public string ManufacturePhone { get; set; }
+        [Column(TypeName = "varchar(254)")]
+        public string ManufactureEmail { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
