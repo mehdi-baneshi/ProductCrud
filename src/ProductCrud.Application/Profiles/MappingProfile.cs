f7bdc44079e06598d0820038e62e0a77b4b7ab28
mehdi-baneshi <mehdibaneshi1373@gmail.com>
2023-06-26 14:52:57 +0330

    add automapper

---
+++ src/ProductCrud.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using ProductCrud.Application.DTOs.Product;
+using ProductCrud.Domain.Entities;
+
+namespace ProductCrud.Application.Profiles
+{
+    public class MappingProfile:Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Product, ProductGetDto>().ReverseMap();
+            CreateMap<Product, ProductCreateDto>().ReverseMap();
+            CreateMap<Product, ProductUpdateDto>().ReverseMap();
+        }
+    }
+}
