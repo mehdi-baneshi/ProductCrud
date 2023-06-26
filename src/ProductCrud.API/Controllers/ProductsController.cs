0bdae5cbd7ab83c04935c0d26636c49161f3b9a5
mehdi-baneshi <mehdibaneshi1373@gmail.com>
2023-06-26 17:43:34 +0330

    add api project and controllers

---
+++ src/ProductCrud.API/Controllers/ProductsController.cs
@@ -0,0 +1,80 @@
+﻿using ProductCrud.Application.DTOs.Product;
+using ProductCrud.Application.Features.Product.Requests.Commands;
+using ProductCrud.Application.Features.Product.Requests.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using ProductCrud.Application.Constants;
+
+namespace ProductCrud.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ProductsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        // GET: api/<ProductsController>
+        [HttpGet]
+        public async Task<ActionResult<List<ProductGetDto>>> Get([FromQuery] ProductFilterDto filterDto)
+        {
+            var products = await _mediator.Send(new GetProductsListQuery
+            {
+                FilterDto = filterDto
+            });
+
+            return Ok(products);
+        }
+
+        // GET api/<PproductsController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductGetDto>> Get(int id)
+        {
+            var product = await _mediator.Send(new GetProductQuery { Id = id });
+
+            return Ok(product);
+        }
+
+        // POST api/<ProductsController>
+        [HttpPost]
+        [Authorize]
+        public async Task<ActionResult> Post([FromBody] ProductCreateDto product)
+        {
+            var command = new CreateProductCommand { ProductDto = product};
+            var repsonse = await _mediator.Send(command);
+
+            return Ok(repsonse);
+        }
+
+        // PUT api/<productsController>
+        [HttpPut]
+        [Authorize]
+        public async Task<ActionResult> Put([FromBody] ProductUpdateDto product)
+        {
+            var command = new UpdateProductCommand { ProductDto = product };
+            await _mediator.Send(command);
+
+            return NoContent();
+        }
+
+        // DELETE api/<productsController>/5
+        [HttpDelete("{id}")]
+        [Authorize]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var command = new DeleteProductCommand { Id = id };
+            await _mediator.Send(command);
+
+            return NoContent();
+        }
+    }
+}
---
+++ src/ProductCrud.API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,72 @@
+﻿using ProductCrud.Application.Exceptions;
+using Microsoft.AspNetCore.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using ProductCrud.Identity.Exceptions;
+
+namespace ProductCrud.Api.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+        private Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            HttpStatusCode statusCode = HttpStatusCode.InternalServerError;
+            string result = JsonSerializer.Serialize(new ErrorDeatils 
+                { 
+                    ErrorMessage = exception.Message, 
+                    ErrorType = "Failure" 
+                });
+
+            switch (exception)
+            {
+                case BadRequestException badRequestException:
+                    statusCode = HttpStatusCode.BadRequest;
+                    break;
+                case ValidationException validationException:
+                    statusCode = HttpStatusCode.BadRequest;
+                    result = JsonSerializer.Serialize(validationException.Errors);
+                    break;
+                case IdentityException identityException:
+                    statusCode = HttpStatusCode.BadRequest;
+                    result = JsonSerializer.Serialize(identityException.Errors);
+                    break;
+                case NotFoundException notFoundException:
+                    statusCode = HttpStatusCode.NotFound;
+                    break;
+                default:
+                    break;
+            }
+
+            context.Response.StatusCode = (int)statusCode;
+            return context.Response.WriteAsync(result);
+        }
+    }
+
+    public class ErrorDeatils
+    {
+        public string ErrorType { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
